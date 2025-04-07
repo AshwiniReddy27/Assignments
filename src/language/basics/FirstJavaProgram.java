d9cf1f5257ba15072212d76ee2f8f654bec9d7ba
Ashwini Reddy <ashwinisahadev27@gmail.com>
2025-04-07 11:23:15 +0530

    Assignments

---
+++ src/language/basics/FirstJavaProgram.java
@@ -0,0 +1,16 @@
+package language.basics;
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println("Hello World!");
+		System.out.println("Step1");
+		System.out.println("Step2");
+		System.out.println("Step3");
+		System.out.println("Step4");
+		System.out.println("Step5"+5/(4+7+3));
+	}
+
+}
