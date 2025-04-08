650a3a8306590b90b3d644940f3a9ba0b9071a1f
Ashwini Reddy <ashwinisahadev27@gmail.com>
2025-04-08 11:53:38 +0530

    Assignment1 Data types is completed

---
+++ src/language/basics/Assignment1_Datatypes.java
@@ -0,0 +1,89 @@
+package language.basics;
+
+public class Assignment1_Datatypes {
+
+	public static void main(String[] args) {
+		//Temperature of a city in degrees Celsius: 25.5 (Datatype is float)
+		float Temperature=25.5f;
+		System.out.println("Temperature of a city in degrees Celsius is "+Temperature);
+		//Whether a customer has placed an order: true or false( Datatype is boolean)
+		boolean order=true;
+		System.out.println(order);
+		//Person's phone number: "123-456-7890" (Datatype is string)
+		String PhoneNumber="123-456-7890";
+		System.out.println("Person's phone number is "+PhoneNumber);
+		//Amount of money in a customer's bank account: 1000.50 (Datatype is double)
+		double money=1000.50;
+		System.out.println("Amount of money in a customer's bank account is "+money);
+		// Person's email address: "john.doe@example.com"(DataType is string)
+		String Emailaddress="john.doe@example.com";
+		System.out.println("Person's email address is "+Emailaddress);
+		//Coordinates of a location (latitude, longitude): 37.7749, -122.4194 (Data Type is float)
+		float latitude=37.7749f;
+		System.out.println("Coordinates of a location is "+latitude +"latitude.");
+		float longitude=-122.4194f;
+		System.out.println("Coordinates of a location is "+longitude +"longitude.");
+		//Person's marital status: true or false (Data type is boolean)
+		boolean maritalStatus=true;
+		System.out.println("Person's marital status is "+maritalStatus);
+		//Person's occupation: "Software Engineer" (data type is string)
+		String occupation="Software Engineer";
+		System.out.println("Person's occupation is "+occupation);
+		//Person's favourite colour: "Blue" (data type is string)
+		String colour="Blue";
+		System.out.println("Person's favourite colour is "+colour);
+		
+		//Current year: 2023(Data type is short);
+		short year =2023;
+		System.out.println("Current year is "+year);
+		//11.Number of followers on a social media platform: 1,000,000 (Data type is string)
+		String followers= "1,000,000";
+		System.out.println("Number of followers on a social media platform is "+followers);
+		//12.Rating of a movie: 7.5( data type is float);
+		float movie=7.5f;
+		System.out.println("Rating of a movie is "+movie);
+		//13.Person's blood type: 'A'(data type is char)
+		char bloodType='A';
+		System.out.println("Person's blood type is " +bloodType);
+		//14.Title of a book: "To Kill a Mockingbird"(data type is string)
+		String book="To Kill a Mockingbird";
+		System.out.println(book);
+		//15.Number of employees in a company: 500(data type is short)
+		short noOfEmp=500;
+		System.out.println("Number of employees in a company is "+noOfEmp);
+		
+		//16.Time of an event: 2:30 PM(data type is string)
+		String eventTime="2:30PM";
+		System.out.println("Time of an event is "+eventTime);
+		
+		//17.Name of a country: "United States"(Data type is string)
+		String nameOfCountry="United States";
+		System.out.println("Name of a country is "+nameOfCountry);
+		//18.Person's eye color: "Brown"(data type is string)
+		String eyeColor="Brown";
+		System.out.println("Person's eye color is " +eyeColor);
+		
+		//19.Person's birthplace: "New York City"(data type is string)
+		String birthPlace="New York City";
+		System.out.println("Person's birthplace is " +birthPlace);
+		//20.Distance between two cities: 200.5 (data type is )
+		float distance=200.5f;
+		System.out.println("Distance between two cities is " +distance);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
--- src/language/basics/FirstJavaProgram.java
+++ src/language/basics/FirstJavaProgram.java
@@ -4,7 +4,11 @@ public class FirstJavaProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello,World!");
+		short marks =110;
+		
+			System.out.println(marks);
+	
 
 	}
-
 }
+
